62200adac18ae4e691ec725be9f8d4aa62533f51
Ruslan Ibragimov <ruslan@ibragimov.by>
2020-03-30 15:10:00 +0300

    Initial release

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 echo "-> Login to docker hub"
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+echo "${DOCKER_TOKEN}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 echo "-> Pushing image heapy/kotlin-docker:${TRAVIS_TAG} to docker hub"
 docker push heapy/kotlin-docker
